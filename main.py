95ef71dbfcd90b0a7b63cf1256732f5df2268e87
lengrab <75564034+lengrab@users.noreply.github.com>
2022-10-12 18:15:43 +0300

    add TOKEN

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-import telebotbot = telebot.TeleBot("5440142515:AAFJKnKnBiL3Ap0FYP_hdx9mB4gH2F-f5WI")@bot.message_handler(commands=['start', 'help'])def send_welcome(message):	bot.reply_to(message, "Howdy, how are you doing?")# @bot.message_handler(func=lambda message: True)# def echo_all(message):#  	bot.reply_to(message, message.text)# @bot.message_handler(func=lambda message: True)# def echo_all(message):#  	bot.send_message(message.chat.id, message.text)bot.infinity_polling()
+import telebot
+
+TOKEN =""
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
+#  	bot.reply_to(message, message.text)
+
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
+#  	bot.send_message(message.chat.id, message.text)
+
+bot.infinity_polling()
