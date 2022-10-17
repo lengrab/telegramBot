8da77ebf5a2adfd6500c3b6d4db8a109de4123ba
lengrab <75564034+lengrab@users.noreply.github.com>
2022-10-17 18:11:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,50 @@
+import logging
+import time
+
+import flask
 import telebot
-from telebot import types
 
-TOKEN = "5440142515:AAFJKnKnBiL3Ap0FYP_hdx9mB4gH2F-f5WI"
+API_TOKEN = ''
+WEBHOOK_PORT = 8444
+WEBHOOK_HOST = '127.0.0.1'
+WEBHOOK_URL = 'https://3eec-77-93-112-174.eu.ngrok.io'
 
-bot = telebot.TeleBot(TOKEN)
-
-markup = types.ReplyKeyboardMarkup(row_width=2)
-item_btn_1 = types.KeyboardButton('Start')
-item_btn_2 = types.KeyboardButton('Help')
-item_btn_3 = types.KeyboardButton('About_author')
-markup.add(item_btn_1, item_btn_2, item_btn_3)
-
-inline_keyboard = types.InlineKeyboardMarkup(row_width=2)
-item_btn_1 = types.InlineKeyboardButton('Start', callback_data='/start')
-item_btn_2 = types.InlineKeyboardButton('Help', callback_data='/help')
-item_btn_3 = types.InlineKeyboardButton('About_author', callback_data='/about_author')
-inline_keyboard.row(item_btn_1,item_btn_2)
-inline_keyboard.row(item_btn_3)
-# inline_keyboard.add(item_btn_1, item_btn_2, item_btn_3)
+logger = telebot.logger
+telebot.logger.setLevel(logging.DEBUG)
+bot = telebot.TeleBot(API_TOKEN)
+app = flask.Flask(__name__)
+time.sleep(0.1)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    # bot.delete_message(message.chat.id, message.id)
-    bot.send_message(message.chat.id, "Привет! Я умею следующие действия:", reply_markup=inline_keyboard)
-
-@bot.callback_query_handler(func=lambda call: True)
-def handle(call):
-    # bot.send_message(call.message.chat.id, 'Data: {}'.format(str(call.data)))
-    if call.data == '/about_author':
-        bot.send_message(call.message.chat.id, 'Hi! My name is Leonid. I\'m author this channel.')
-    if call.data == '/help':
-        bot.send_message(call.message.chat.id, 'How can I help you?')
-    bot.answer_callback_query(call.id)
+    bot.reply_to(message,
+                 ("Hi there, I am EchoBot.\n"
+                  "I am here to echo your kind words back to you."))
 
 
-
-# @bot.message_handler(commands=['help'])
-# def send_welcome(message):
-#     bot.reply_to(message, "Howdy, how are you doing?")
-
-@bot.message_handler(commands=['help'])
-def send_welcome(message):
-    bot.send_message(message.chat.id, "text", reply_markup=inline_keyboard)
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 
-@bot.message_handler(commands=['about_author'])
-def send_about(message):
-    bot.send_message(message.chat.id, 'Hi! My name is Leonid. I\'m author this channel.')
+# Process webhook calls
+@app.route('/' + API_TOKEN, methods=['POST', 'GET'])
+def webhook():
+    logger.debug("gas")
+    logger.debug(flask.request.get_data())
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+
+        return '200'
+    else:
+        flask.abort(403)
 
 
-# @bot.message_handler(func=lambda message: True)
-# def echo_all(message):
-#  	bot.reply_to(message, message.text)
-
-# @bot.message_handler(func=lambda message: True)
-# def echo_all(message):
-#  	bot.send_message(message.chat.id, message.text)
-
-bot.infinity_polling()
-input()
+bot.remove_webhook()
+bot.set_webhook(WEBHOOK_URL + "/" + API_TOKEN)
+app.run(host=WEBHOOK_HOST,
+        port=WEBHOOK_PORT)
